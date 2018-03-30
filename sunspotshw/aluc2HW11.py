0827ce75f7b28e84e9a543c35f37d032833e0d18
desktopdrew <aluc2@asu.edu>
2018-03-30 08:32:01 -0700

    I think this is the right answer for the code. Still needs annotation.

--- sunspotshw/aluc2HW11.py
+++ sunspotshw/aluc2HW11.py
@@ -2,38 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-## A
+# Load sunspot data
 months,spots = np.loadtxt('sunspots.txt', delimiter = '	', usecols=(0,1), unpack = True)
-
 months = months.astype(int)
 
-
-
-#time_step = 1/44100 # 44100 samples per second
-
+# Plot the Sun Spot signal
 plt.plot(months, spots)
 plt.xlabel('time (months)')
 plt.ylabel('Sun Spots')
 plt.show()
 
-cycle = 148-10 # Estimate via speculation, finding the difference in time between first two peaks
-print(cycle)
+# Estimate via speculation, finding the difference in time between first two peaks(Time Domain)
+cycle = 148-10
+print("Sunspot Period (Initial Estimate) : " + str(cycle))
+
 
 ## B
-#perform a fast Fourier transform
+# Perform a fast Fourier transform on the signal
 from scipy import fftpack
-sample_freq = fftpack.fftfreq(spots.size)#, d=time_step)
+sample_freq = fftpack.fftfreq(spots.size)
 spots_fft = fftpack.fft(spots)
 
-#Since the signal is real, just plot the postive frequencies
+# Omit negative frequencies
 pidxs = np.where(sample_freq > 0)
 freqs = sample_freq[pidxs]
 power = np.square(np.abs(spots_fft)[pidxs])
-#powerdB = np.log10(power)
+
+# Plotting the signal in the frequency domain
 plt.plot(freqs[1:1000],power[1:1000])
 plt.xlabel('k')
 plt.ylabel('|ck|^2')
 plt.show()
 
-cycle = 1/0.007636  #taking the inverse of the peak frequency
-print(cycle)
+fft_cycle = 1/0.007636  #taking the inverse of the peak frequency
+print("Sunspot Period (From fft) : " + str(fft_cycle))
